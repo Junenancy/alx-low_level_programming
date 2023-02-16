f227566d976e3dcc296f78c267bd681eb398d41d
Iroka Juliet <irokajuliet01@gmail.com>
2023-02-16 13:44:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - a program that prints a line with puts function
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programing is like building a multilingual puzzle");
+return (0);
+}
