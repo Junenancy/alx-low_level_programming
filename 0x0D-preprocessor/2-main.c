c942db65c5092e070c6f2f69642b6fa32e5ca306
Iroka Juliet <irokajuliet01@gmail.com>
2023-03-20 08:46:37 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *filename = __FILE__;
+
+	while (*filename != '\0')
+	{
+		_putchar(*filename);
+		filename++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
