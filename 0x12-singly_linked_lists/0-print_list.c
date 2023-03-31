b8c1bc91f0b6323a5a5054431df12a79786a22b9
Junenancy <irokajuliet01@gmail.com>
2023-03-31 15:39:28 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
-#include <stdio.h>`
-#include "list.h"
+#include <stdio.h>
+#include "lists.h"
 
 /**
  * print_list - prints all the elements of a linked list
@@ -8,19 +8,20 @@
  * Return: the number of nodes printed
  */
 
-size_t_ print_list(const list_t *h);
+size_t print_list(const list_t *h)
+
 {
 	size_t s = 0;
 
 		while (h)
 		{
 			if (!h->str)
-				printf("[0](nil)\n";
+				printf("[0] (nil)\n");
 				else
 					printf("[%u] %s\n", h->len, h->str);
-						h = h->next
+						h = h->next;
 						s++;
-						}
+		}
 
 						return (s);
 						}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX SE project
+ * for Holberton project
  */
 typedef struct list_s
 {
